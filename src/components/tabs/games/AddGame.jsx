48436906757bd74044c071b51d262cc41a2f916b
unknown <souissi13mouldi@gmail.com>
2023-02-23 22:56:30 +0100

    add

--- src/components/tabs/games/AddGame.jsx
+++ src/components/tabs/games/AddGame.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import useGlobalStore from "../../../strore";
+import CustomSelect from "../../CustomSelect";
 
 const AddGame = () => {
   const [data, setData] = useState({ type: "utilisateur" });
@@ -15,9 +16,13 @@ const AddGame = () => {
     addUser(data);
     refClose.current.click();
   };
+
+  const handleTariffType = (name, value) => {
+    console.log({ name, value });
+  };
   return (
     <div className="modal fade" id="addGame" tabIndex="-1" aria-hidden="true">
-      <div className="modal-dialog modal-xl">
+      <div className="modal-dialog modal-fullscreen">
         <form onSubmit={handleSubmit}>
           <div className="modal-content">
             <div className="modal-header">
@@ -30,17 +35,46 @@ const AddGame = () => {
               ></button>
             </div>
             <div className="modal-body">
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Nom"
-                  name="name"
-                  onChange={handleInput}
-                  required
-                  autoComplete="off"
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="form-floating mb-3 ">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Nom"
+                      name="name"
+                      onChange={handleInput}
+                      required
+                      autoComplete="off"
+                    />
+                    <label>Nom</label>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-floating mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Nom"
+                      name="logo"
+                      onChange={handleInput}
+                    />
+                    <label>Logo</label>
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex align-items-center p-4">
+                <div className="fw-bold me-3">Tarif</div>
+                <button className="btn btn-outline-primary py-0 px-1">
+                  <i className="bi bi-plus h3"></i>
+                </button>
+              </div>
+              <div className="p-4">
+                <label className="mb-2">Mode de paiement</label>
+                <CustomSelect
+                  options={["Par heure", "Par match"]}
+                  getSelected={handleTariffType}
                 />
-                <label>Nom</label>
               </div>
               <div className="form-floating mb-3">
                 <input
@@ -51,12 +85,6 @@ const AddGame = () => {
                   onChange={handleInput}
                 />
                 <label>Logo</label>
-              </div>
-              <div className="d-flex align-items-center p-4">
-                <div className="fw-bold me-3">Tarif</div>
-                <button className="btn btn-outline-primary py-0 px-1">
-                  <i className="bi bi-plus h3"></i>
-                </button>
               </div>
             </div>
             <div className="modal-footer">
